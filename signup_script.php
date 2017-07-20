35a81780add7f48429fab531292ecfff3383a8e3
Tanavya <tanavya.dimri@gmail.com>
2017-07-20 17:32:46 +0530

    Added signup page and signup scripts.

---
+++ signup_script.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'database_handler.php';
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$uid = $_POST['uid'];
+$email = $_POST['email'];
+$pwd = $_POST['pwd'];
+$class = $_POST['class'];
+
+/*echo $first . $last . $uid . $class . $pwd . $class;*/
+$sql = "INSERT INTO user_table (first, last, uid, email, pwd, class)
+VALUES ('$first', '$last', '$uid', '$email', '$pwd', '$class')";
+
+$result = mysqli_query($conn, $sql);
+
+header("Location: index.html");
+
+?>
