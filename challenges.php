fbbbdb96d8dc4812cd657b643190392833d1308c
Tanavya <tanavya.dimri@gmail.com>
2017-07-27 00:12:26 +0530

    Updated challenges page to tabs and stuff.

--- challenges.php
+++ challenges.php
@@ -23,7 +23,42 @@
 				</div>
 			</div>		
 		</div>
-
+		<div class="col-md-12 animate-box" style="margin-top:140px; height:100em;">
+			<div id="fh5co-tab-feature-vertical" class="fh5co-tab">
+				<ul class="resp-tabs-list hor_1">
+					<li>Challenge Problem</li>
+					<li>Leaderboard</li>
+				</ul>
+				<div class="resp-tabs-container hor_1">
+					<div>
+						<div class="row">
+							<div class="col-md-12">
+								<h2 class="h3">Challenge Problem</h2>
+							</div>
+							<div class="col-md-6" style = "width: 100%;">
+								<iframe src="
+								https://docs.google.com/forms/d/e/1FAIpQLSc02zXEfgshWgEUBR0ZfrkFjTWq2NyW9S702yDaRfVXVrAnWw/viewform?c=0&w=1&includes_info_params=true
+								&widget=false&headers=false&chrome=false" 
+								style = "width: 100%; height: 80em;"></iframe>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div class="row">
+							<div class="col-md-12">
+								<h2 class="h3">Leaderboard</h2>
+							</div>
+							<div class="col-md-6" style = "width: 100%;">
+								<p>The leaderboard will be updated after your answers are checked.</p>
+								<iframe src="https://docs.google.com/spreadsheets/d/15vTtDTwAlrfmGUDNv2h6JuRXBF-zrv04NyIsULgRfXY/pubhtml?widget=false&headers=false&chrome=false" style = "width: 100%; height: 1000px;"></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+					
+		<!--
 		<div id="fh5co-main">
 			<div class="container" style = "width:79%;">
 				<div class="row">
@@ -37,9 +72,8 @@
 <iframe src="https://docs.google.com/spreadsheets/d/15vTtDTwAlrfmGUDNv2h6JuRXBF-zrv04NyIsULgRfXY/pubhtml?widget=false&headers=false&chrome=false" style = "width: 100%; height: 1000px;"></iframe>
 				</div>
 			</div>
-
-		
 		</div>
+		-->
 		<!-- END fhtco-main -->
 
 
